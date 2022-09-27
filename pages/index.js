39862aea6f3cf8f62210667ec94b74f9ff7b67d1
diah pramesti <dpanbers@gmail.com>
2022-09-27 22:21:31 +0800

    edit file

--- pages/index.js
+++ pages/index.js
@@ -3,40 +3,44 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { useEffect, useState } from 'react'
 
-
-export default function Home() {
-  const [comments, setComments] = useState([])
-
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/comments')
-  })
-
+export default function Comments(props) {
   return (
-    <div className={styles.container}>
+    <div>
+      <h1>USE SSR</h1>
       <table>
-        <thead>
-          <tr>
-            <th>Post ID</th>
-            <th>ID</th>
-            <th>NAME</th>
-            <th>EMAIL</th>
-            <th>BODY</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            comments.map(comments => (
-              <tr key={comments.id}>
-                <td>{comments.postId}</td>
-                <td>{comments.id}</td>
-                <td>{comments.name}</td>
-                <td>{comments.email}</td>
-                <td>{comments.body}</td>
+          <thead>
+              <tr>
+                  <th>POST ID</th>
+                  <th>ID</th>
+                  <th>NAME</th>
+                  <th>EMAIL</th>
+                  <th>BODY</th>
               </tr>
-            ))
-          }
-        </tbody>
+          </thead>
+          <tbody>
+              {
+                  props.comments?.map(comments => (
+                      <tr key={comments.id}>
+                          <td>{comments.postId}</td>
+                          <td>{comments.id}</td>
+                          <td>{comments.name}</td>
+                          <td>{comments.email}</td>
+                          <td>{comments.body}</td>
+                      </tr>
+                  ))
+              }
+          </tbody>
       </table>
     </div>
+      
   )
+}
+
+export async function getServerSideProps(context) {
+  const res = await fetch('https://jsonplaceholder.typicode.com/comments')
+  const comments = await res.json();
+
+  return {
+      props: {comments}
+  }
 }
